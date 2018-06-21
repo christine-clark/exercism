a726fef001a4594b2772b8b27c10ffb4cb2e015d
Christine Clark <chinita96@gmail.com>
2018-06-21 10:03:20 -0400

    Add gitignore and updated acronym to ES6.

--- javascript/acronym/acronym.js
+++ javascript/acronym/acronym.js
@@ -1,24 +1,22 @@
 'use strict';
 
-function Acronyms() {
-  return {
-    parse: function(phrase) {
-      if (!phrase || typeof phrase !== 'string') {
-        throw new Error('Phrase must be a string');
-      }
-
-      var acronym = [];
-      var words = phrase.split(/[\s-]|[a-z](?=[A-Z])/);
-
-      if (words.length) {
-        acronym = words.map(function(word) {
-          return word.charAt(0).toUpperCase();
-        });
-      }
-      
-      return acronym.join('');
+const Acronyms = {
+  parse: (phrase) => {
+    if (!phrase || typeof phrase !== 'string') {
+      throw new Error('Phrase must be a string');
     }
-  };
+
+    let acronym = [];
+    const words = phrase.split(/[\s-]|[a-z](?=[A-Z])/);
+
+    if (words.length) {
+      acronym = words.map((word) => {
+        return word.charAt(0).toUpperCase();
+      });
+    }
+
+    return acronym.join('');
+  }
 };
 
-module.exports = Acronyms();
+module.exports = Acronyms;
