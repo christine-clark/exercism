6f8d31626b0374fb4a6599427313d9fa13deb75d
Christine Clark <chinita96@gmail.com>
2018-06-21 10:16:33 -0400

    Convert to ES6.

--- javascript/food-chain/food-chain.js
+++ javascript/food-chain/food-chain.js
@@ -1,71 +1,72 @@
 'use strict';
 
-function FoodChain() {
-  this.songLines = [
-    ['fly', "I don't know why she swallowed the fly. Perhaps she'll die."],
-    ['spider', "It wriggled and jiggled and tickled inside her."],
-    ['bird', "How absurd to swallow a bird!"],
-    ['cat', "Imagine that, to swallow a cat!"],
-    ['dog', "What a hog, to swallow a dog!"],
-    ['goat', "Just opened her throat and swallowed a goat!"],
-    ['cow', "I don't know how she swallowed a cow!"],
-    ['horse', "She's dead, of course!"]
-  ];
-};
-
-FoodChain.prototype.verse = function(line) {
-  if (!line || typeof line !== 'number') {
-    throw new Error('Verse must be a number');
+class FoodChain {
+  constructor() {
+    this.songLines = [
+      ['fly', "I don't know why she swallowed the fly. Perhaps she'll die."],
+      ['spider', "It wriggled and jiggled and tickled inside her."],
+      ['bird', "How absurd to swallow a bird!"],
+      ['cat', "Imagine that, to swallow a cat!"],
+      ['dog', "What a hog, to swallow a dog!"],
+      ['goat', "Just opened her throat and swallowed a goat!"],
+      ['cow', "I don't know how she swallowed a cow!"],
+      ['horse', "She's dead, of course!"]
+    ];
   }
 
-  var lineIndex = line - 1;
-  var fullVerse = [
-    'I know an old lady who swallowed a ' + 
-    this.songLines[lineIndex][0] + 
-    '.'
-  ];
+  verse(line) {
+    if (!line || typeof line !== 'number') {
+      throw new Error('Verse must be a number');
+    }
 
-  if (line === 1 || line === 8) {
-    fullVerse.splice(1, 0, this.songLines[lineIndex][1]);
-  } else {
-    for (var i = 0; i <= lineIndex; i++) {
-      var tailLine = i === 1 ? 
-        ' that wriggled and jiggled and tickled inside her.' : 
-        '.';
+    const lineIndex = line - 1;
+    let fullVerse = [
+      'I know an old lady who swallowed a ' +
+      this.songLines[lineIndex][0] +
+      '.'
+    ];
 
-      if (i === 0 || i === lineIndex) {
-        fullVerse.splice(1, 0, this.songLines[i][1]);
-      }
+    if (line === 1 || line === 8) {
+      fullVerse.splice(1, 0, this.songLines[lineIndex][1]);
+    } else {
+      for (let i = 0; i <= lineIndex; i++) {
+        const tailLine = i === 1 ?
+          ' that wriggled and jiggled and tickled inside her.' :
+          '.';
 
-      if (i !== lineIndex) {
-        fullVerse.splice(1, 0, 
-          'She swallowed the ' + 
-          this.songLines[i+1][0] + 
-          ' to catch the ' + 
-          this.songLines[i][0] + 
-          tailLine);
+        if (i === 0 || i === lineIndex) {
+          fullVerse.splice(1, 0, this.songLines[i][1]);
+        }
+
+        if (i !== lineIndex) {
+          fullVerse.splice(1, 0,
+            'She swallowed the ' +
+            this.songLines[i + 1][0] +
+            ' to catch the ' +
+            this.songLines[i][0] +
+            tailLine);
+        }
       }
     }
+
+    fullVerse.push('');
+    return fullVerse.join('\n');
   }
 
-  fullVerse.push('');
-  return fullVerse.join('\n');
-};
+  verses(startVerse, endVerse) {
+    if (!startVerse || typeof startVerse !== 'number' ||
+      !endVerse || typeof endVerse !== 'number') {
+      throw new Error('Verses must be a number');
+    }
 
-FoodChain.prototype.verses = function(startVerse, endVerse) {
-  if (!startVerse || typeof startVerse !== 'number' || 
-    !endVerse || typeof endVerse !== 'number') {
-    throw new Error('Verses must be a number');
+    let fullSong = [];
+    for (let i = startVerse; i <= endVerse; i++) {
+      fullSong.push(this.verse(i));
+    }
+
+    fullSong.push('');
+    return fullSong.join('\n');
   }
-
-  var fullSong = [];
-
-  for (var i = startVerse; i <= endVerse; i++) {
-    fullSong.push(this.verse(i));
-  }
-
-  fullSong.push('');
-  return fullSong.join('\n');
 };
 
 module.exports = FoodChain;
--- javascript/secret-handshake/secret-handshake.js
+++ javascript/secret-handshake/secret-handshake.js
@@ -1,31 +1,33 @@
 'use strict';
 
-function SecretHandshake(handshake) {
-  if (!handshake || typeof handshake !== 'number') {
-    throw new Error('Handshake must be a number');
-  }
-
-  this.handshake = handshake;
-};
-
-SecretHandshake.prototype.commands = function() {
-  var binary = Number(this.handshake).toString(2);
-  var binaryDigits = binary.split('').reverse();
-  var commandsList = ['wink', 'double blink', 'close your eyes', 'jump', 'reverse'];
-  var finalCommands = [];
-  var index = 0;
-
-  while (index < binaryDigits.length) {
-    if (commandsList[index] === 'reverse') {
-      finalCommands = finalCommands.reverse();
-    } else if (binaryDigits[index] === '1') {
-      finalCommands.push(commandsList[index]);
+class SecretHandshake {
+  constructor(handshake) {
+    if (!handshake || typeof handshake !== 'number') {
+      throw new Error('Handshake must be a number');
     }
 
-    index++;
+    this.handshake = handshake;
   }
 
-  return finalCommands;
+  commands() {
+    const binary = Number(this.handshake).toString(2);
+    const binaryDigits = binary.split('').reverse();
+    const commandsList = ['wink', 'double blink', 'close your eyes', 'jump', 'reverse'];
+    let finalCommands = [];
+    let index = 0;
+
+    while (index < binaryDigits.length) {
+      if (commandsList[index] === 'reverse') {
+        finalCommands = finalCommands.reverse();
+      } else if (binaryDigits[index] === '1') {
+        finalCommands.push(commandsList[index]);
+      }
+
+      index++;
+    }
+
+    return finalCommands;
+  }
 };
 
 module.exports = SecretHandshake;
