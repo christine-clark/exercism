d6420c9dead476134e1a94e435ffede57253814f
Christine Clark <chinita96@gmail.com>
2018-06-21 12:15:16 -0400

    Moved logic to private method to get verse.

--- javascript/food-chain/food-chain.js
+++ javascript/food-chain/food-chain.js
@@ -1,8 +1,8 @@
 'use strict';
 
-class FoodChain {
-  constructor() {
-    this.songLines = [
+const Song = {
+  getVerse(line) {
+    const songLines = [
       ['fly', "I don't know why she swallowed the fly. Perhaps she'll die."],
       ['spider', "It wriggled and jiggled and tickled inside her."],
       ['bird', "How absurd to swallow a bird!"],
@@ -12,22 +12,15 @@ class FoodChain {
       ['cow', "I don't know how she swallowed a cow!"],
       ['horse', "She's dead, of course!"]
     ];
-  }
-
-  verse(line) {
-    if (!line || typeof line !== 'number') {
-      throw new Error('Verse must be a number');
-    }
 
     const lineIndex = line - 1;
     let fullVerse = [
-      'I know an old lady who swallowed a ' +
-      this.songLines[lineIndex][0] +
-      '.'
+      'I know an old lady who swallowed a ' + 
+      songLines[lineIndex][0] + '.'
     ];
 
     if (line === 1 || line === 8) {
-      fullVerse.splice(1, 0, this.songLines[lineIndex][1]);
+      fullVerse.splice(1, 0, songLines[lineIndex][1]);
     } else {
       for (let i = 0; i <= lineIndex; i++) {
         const tailLine = i === 1 ?
@@ -35,22 +28,30 @@ class FoodChain {
           '.';
 
         if (i === 0 || i === lineIndex) {
-          fullVerse.splice(1, 0, this.songLines[i][1]);
+          fullVerse.splice(1, 0, songLines[i][1]);
         }
 
         if (i !== lineIndex) {
-          fullVerse.splice(1, 0,
-            'She swallowed the ' +
-            this.songLines[i + 1][0] +
-            ' to catch the ' +
-            this.songLines[i][0] +
-            tailLine);
+          fullVerse.splice(1, 0, 
+            'She swallowed the ' + songLines[i + 1][0] +
+            ' to catch the ' + songLines[i][0] + tailLine
+          );
         }
       }
     }
 
     fullVerse.push('');
     return fullVerse.join('\n');
+  }
+}
+
+class FoodChain {
+  verse(line) {
+    if (!line || typeof line !== 'number') {
+      throw new Error('Verse must be a number');
+    }
+
+    return Song.getVerse(line);
   }
 
   verses(startVerse, endVerse) {
@@ -61,7 +62,7 @@ class FoodChain {
 
     let fullSong = [];
     for (let i = startVerse; i <= endVerse; i++) {
-      fullSong.push(this.verse(i));
+      fullSong.push(Song.getVerse(i));
     }
 
     fullSong.push('');
