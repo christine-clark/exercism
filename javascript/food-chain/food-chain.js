eb2a853538fdf9d74ea2ad62c233e1b1099c30ac
Christine Clark <chinita96@gmail.com>
2018-06-20 20:33:20 -0400

    Adjusting constructor function and uncommenting missed unit test.

--- javascript/food-chain/food-chain.js
+++ javascript/food-chain/food-chain.js
@@ -1,15 +1,7 @@
 'use strict';
 
 function FoodChain() {
-  // Constructor function
-};
-
-FoodChain.prototype.verse = function(line) {
-  if (!line || typeof line !== 'number') {
-    throw new Error('Verse must be a number');
-  }
-
-  var songLines = [
+  this.songLines = [
     ['fly', "I don't know why she swallowed the fly. Perhaps she'll die."],
     ['spider', "It wriggled and jiggled and tickled inside her."],
     ['bird', "How absurd to swallow a bird!"],
@@ -19,22 +11,28 @@ FoodChain.prototype.verse = function(line) {
     ['cow', "I don't know how she swallowed a cow!"],
     ['horse', "She's dead, of course!"]
   ];
+};
+
+FoodChain.prototype.verse = function(line) {
+  if (!line || typeof line !== 'number') {
+    throw new Error('Verse must be a number');
+  }
 
   var lineIndex = line - 1;
-  var fullVerse = ['I know an old lady who swallowed a ' + songLines[lineIndex][0] + '.'];
+  var fullVerse = ['I know an old lady who swallowed a ' + this.songLines[lineIndex][0] + '.'];
 
   if (line === 1 || line === 8) {
-    fullVerse.splice(1, 0, songLines[lineIndex][1]);
+    fullVerse.splice(1, 0, this.songLines[lineIndex][1]);
   } else {
     for (var i = 0; i <= lineIndex; i++) {
       var tailLine = i === 1 ? ' that wriggled and jiggled and tickled inside her.' : '.';
 
       if (i === 0 || i === lineIndex) {
-        fullVerse.splice(1, 0, songLines[i][1]);
+        fullVerse.splice(1, 0, this.songLines[i][1]);
       }
 
       if (i !== lineIndex) {
-        fullVerse.splice(1, 0, 'She swallowed the ' + songLines[i+1][0] + ' to catch the ' + songLines[i][0] + tailLine);
+        fullVerse.splice(1, 0, 'She swallowed the ' + this.songLines[i+1][0] + ' to catch the ' + this.songLines[i][0] + tailLine);
       }
     }
   }
