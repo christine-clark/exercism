c11f11c48cd529a21fcaa66fea1f00ce831b869d
Christine Clark <chinita96@gmail.com>
2020-01-25 17:47:18 -0500

    Added ruby exercism for grade school.

---
+++ ruby/grade-school/grade_school.rb
@@ -0,0 +1,21 @@
+class School
+  def initialize
+    @students = Hash.new { |hash, key| hash[key] = [] }
+  end
+
+  def add(student, grade)
+    @students[grade].push(student)
+  end
+
+  def students(grade)
+    @students[grade].sort
+  end
+
+  def students_by_grade
+    return [] if @students.empty?
+
+    @students.sort.map do |grade, students|
+      { grade: grade, students: students.sort }
+    end
+  end
+end
